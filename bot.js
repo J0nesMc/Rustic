8e3f5ecba60c6233dd64cba226830af2ffef341c
J0nesMc <60965101+J0nesMc@users.noreply.github.com>
2020-02-12 19:29:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord - require('discord.js');
+const client - new Discord.Client();
 
 client.on('ready', () => {
   console.log(`I am ready`);
 });
 
-client.on('message', msg => {
-  if (message.content === 'ping') {
+client.on('message', (message) => {
+  if (message.content === 'ping'){
       message.reply('pong');
       }
-});
+}});
 
 
 // THIS MUST BE THIS WAY
-client.login('process.env.0fYXNt24HwVUZdmiH8R9cKyGY5QtBIc-');
+client.login('process.env.token');
